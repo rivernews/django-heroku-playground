57f4fe082d53df9f0dfc92463c19be48bf723256
Shaung Cheng <shaungc@SC-C02XQ5Q7JG5J.attlocal.net>
2020-10-09 01:07:20 -0700

    inject react app into django's frontend app

--- django_project/frontend/views.py
+++ django_project/frontend/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class ReactView(TemplateView):
-    template_name = 'frontend/index.html'
+    template_name = 'index.html'
