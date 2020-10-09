cad4111f09b5528833969cd142a91f83dbe273c4
Shaung Cheng <shaungc@SC-C02XQ5Q7JG5J.attlocal.net>
2020-10-09 00:23:23 -0700

    commit all static assets

---
+++ django_project/frontend/static/precache-manifest.64124b7d4b0ef85679002dc88b0c55cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a33c93e82ce16426c5937a803c08a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803a30f3b544ea65acba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c05c8839468a275a366",
+    "url": "/static/js/2.fc383eab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc383eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803a30f3b544ea65acba",
+    "url": "/static/js/main.f99380e3.chunk.js"
+  },
+  {
+    "revision": "77785b5d7f106e80a267",
+    "url": "/static/js/runtime-main.5bebf6b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
