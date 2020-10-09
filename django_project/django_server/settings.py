43fe3df96299a7c835508248c16c8a127e00c2ab
Shaung Cheng <shaungc@SC-C02XQ5Q7JG5J.attlocal.net>
2020-10-09 11:05:04 -0700

    try to see what exactly cause staticfile to not working on heroku

--- django_project/django_server/settings.py
+++ django_project/django_server/settings.py
@@ -136,7 +136,14 @@ STATICFILES_DIRS = [
 # Configure Django App for Heroku.
 # https://stackoverflow.com/a/61941034/9814131
 # https://github.com/pkrefta/django-on-heroku
-if os.environ.get('HEROKU') == 'yes':
-    import django_on_heroku
-    django_on_heroku.settings(locals())
+# if os.environ.get('HEROKU') == 'yes':
+#     import django_on_heroku
+#     django_on_heroku.settings(locals())
+
+ALLOWED_HOSTS += [
+    'django-heroku-playground.herokuapp.com',
+    'localhost',
+    '127.0.0.1'
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
