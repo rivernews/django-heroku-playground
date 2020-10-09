817377040a6778f94ed8c39f4839da946f0d728d
Shaung Cheng <shaungc@SC-C02XQ5Q7JG5J.attlocal.net>
2020-10-08 23:44:39 -0700

    fix routing args

--- django_project/django_server/frontend/urls.py
+++ django_project/django_server/frontend/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import ReactView
 
 urlpatterns = [
-    path('', include('frontend.urls')),
+    path('', ReactView.as_view()),
 ]
